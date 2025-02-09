53c91a3048a38893792f03edd8e9aa2776220889
Dipan Chakraborty <dipan2003.dev@gmail.com>
2025-02-10 02:04:57 +0530

    Chatflow- A Realtime chat Application

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,9 +33,13 @@ app.use(cookieParser());
 
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../client/build")));
+    app.use(express.static(path.join(__dirname, "../client/dist")));
 
 }
+
+app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+})
 
 
 
